0a8ccb8636d3cca851f37b6eaafbc020fb76b910
emmanuel <badejoemmanuel805@gmail.com>
2025-02-11 09:36:09 -0800

    Created a util function to greet user according to the time of the day

--- src/components/Analytic.jsx
+++ src/components/Analytic.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { UserContext } from './ProtectRoute'
+import { getGreeting } from '../lib/utlis'
 
 
 const Analytic = () => {
@@ -7,10 +8,11 @@ const Analytic = () => {
 
       return (
 
-            <div className='border-border shadow   bg-card   border text-muted-foreground flex  justify-between  p-6 rounded-xl md:min-h-[9rem] max-w-[1200px]'>
+            <div className='border-border shadow   bg-card   border text-muted-foreground flex  justify-between  p-4 md:p-6 rounded-xl md:min-h-[9rem] max-w-[1200px]'>
                   <div className='flex flex-col h-full justify-between'>
                         <div className='space-y-4'>
-                              <h2 className='text-secondary-foreground font-semibold tracking-wider max-sm:text-xl md:text-3xl'>Welcome back, <span className='text-primary'> {user?.username}</span></h2>
+                              <h2 className='text-secondary-foreground font-semibold sm:tracking-wider max-sm:text-xl md:text-3xl'>{getGreeting()}, <span className='text-primary'> {user?.username}</span></h2>
+                              <p className='max-sm:text-xs'>Welcome back to Cloud<span className='text-muted-foreground'>store</span> dashboard!</p>
                               {/* <p className='text-muted-foreground leading-5'>Lorem ipsum dolor numquam non corporis vero. Ipsa saepe corrupti impedit.</p> */}
                         </div>
                   </div>
@@ -20,11 +22,7 @@ const Analytic = () => {
                         </div>
                         <h2 className='text-slate-600/90 text-base'>1.4MB of 2GB used</h2>
                   </div> */}
-
-
-
             </div>
       )
 }
-
 export default Analytic
--- src/lib/utlis.js
+++ src/lib/utlis.js
@@ -1,3 +1,17 @@
 export function GETNENV(name) {
     return import.meta.env["VITE_" + name];
 }
+
+export function getGreeting() {
+    const hour = new Date().getHours();
+
+    if (hour >= 5 && hour < 12) {
+        return "Good morning";
+    } else if (hour >= 12 && hour < 17) {
+        return "Good afternoon";
+    } else if (hour >= 17 && hour < 21) {
+        return "Good evening";
+    } else {
+        return "Good night";
+    }
+}
