30651e047a62fe31966ed544b7b98d38d02d5916
emmanuel <badejoemmanuel805@gmail.com>
2025-02-13 09:08:53 -0800

    Changed loading spinner

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { UserContext } from './ProtectRoute'
 import Search from './Search'
 import { useNavigate } from 'react-router-dom'
-import Mobile from './ui/Mobile'
 
 const DashboardHeader = ({ setTheme, theme, setState }) => {
       const light = theme == "light"
@@ -29,7 +28,7 @@ const DashboardHeader = ({ setTheme, theme, setState }) => {
                               localStorage.removeItem("token");
                               navigate("/auth/login", { replace: true })
 
-                        }} className='cursor-pointer  text-popover-foreground p-2 rounded-full'>
+                        }} className='cursor-pointer  text-destructive p-2 rounded-full'>
                               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
                         </div>
                         <div className='h-full aspect-square cursor-pointer rounded-full bg-accent to-transparent' />
--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -9,8 +9,8 @@ export const UserContext = createContext({ user: null })
 const ProtectRoute = ({ children }) => {
       const { data, error, isPending } = useAuth()
       if (isPending) {
-            return <div className="h-screen w-full text-center text-xl flex items-center">
-                  Loading...
+            return <div className="h-screen text-secondary-foreground/90 bg-stone-200/40  w-full text-center justify-center text-xl flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="49" height="49" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide animate-spin lucide-loader"><path d="M12 2v4" /><path d="m16.2 7.8 2.9-2.9" /><path d="M18 12h4" /><path d="m16.2 16.2 2.9 2.9" /><path d="M12 18v4" /><path d="m4.9 19.1 2.9-2.9" /><path d="M2 12h4" /><path d="m4.9 4.9 2.9 2.9" /></svg>
             </div>
       }
       if (!data) {
