a28563819c19c0c4d1edfdabd62ff41344de62cd
emmanuel <badejoemmanuel805@gmail.com>
2025-02-13 05:39:13 -0800

    Fixed the background opacity of Sidebar mobile screen

--- src/components/sidebarmobile.jsx
+++ src/components/sidebarmobile.jsx
@@ -14,7 +14,7 @@ const SideBarMobile = ({ state, setState }) => {
       }, [state])
       return (
             <>
-                  <div className={`fixed  bg-accent/30 flex flex-col py-4 px-3 top-0 transition-transform duration-300 bottom-0 z-[9999999] bg-acce w-[65%] shadow-ring ${!state && "translate-x-[-1000%]"}`}>
+                  <div className={`fixed  bg-accent flex flex-col py-4 px-3 top-0 transition-transform duration-300 bottom-0 z-[9999999] bg-acce w-[65%] shadow-ring ${!state && "translate-x-[-1000%]"}`}>
                         <div className='text-2xl tracking-wider px-4 text-left text-accent-foreground'>Cloud
                               <span className='text-primary'>
                                     Store
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,35 +1,35 @@
 const useFetch = (endpoint) => {
-    const fetchUser = async (methods, _token) => {
-        const token = _token ?? localStorage.getItem("token");
+      const fetchUser = async (methods, _token) => {
+            const token = _token ?? localStorage.getItem("token");
 
-        let response, data;
+            let response, data;
 
-        try {
-            response = await fetch(endpoint, {
-                headers: { Authorization: `Token ${token}` },
-                ...methods,
-            });
-
-            data = await response.json();
-        } catch (error) {
-            throw new Error("Network error. Please try again later.");
-        }
-
-        if (!response.ok) {
-            let stringifiedData;
             try {
-                stringifiedData = JSON.stringify(data);
+                  response = await fetch(endpoint, {
+                        headers: { Authorization: `Token ${token}` },
+                        ...methods,
+                  });
+
+                  data = await response.json();
             } catch (error) {
-                throw new Error(`Error ${response.status}: Unable to parse response.`);
+                  throw new Error("Network error. Please try again later.");
             }
 
-            throw new Error(`Error ${response.status}: ${stringifiedData}`);
-        }
+            if (!response.ok) {
+                  let stringifiedData;
+                  try {
+                        stringifiedData = JSON.stringify(data);
+                  } catch (error) {
+                        throw new Error(`Error ${response.status}: Unable to parse response.`);
+                  }
 
-        return data;
-    };
+                  throw new Error(`Error ${response.status}: ${stringifiedData}`);
+            }
 
-    return { fetchUser };
+            return data;
+      };
+
+      return { fetchUser };
 };
 
 export default useFetch;
