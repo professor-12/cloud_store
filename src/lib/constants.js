adcc1ed59c786034ba2586fdb8687cad2fdc94d1
emmanuel <badejoemmanuel805@gmail.com>
2025-02-20 12:18:59 -0800

    Add link to google auth to the login page

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,14 @@
 import { GETNENV } from "./utlis";
-
 export const BASE_URL =
     import.meta.env.MODE !== "development"
         ? GETNENV("SERVER_URL")
         : "http://localhost:8000/";
+
+const redirect_uri =
+    import.meta.env.MODE == "development"
+        ? "http://localhost:5173"
+        : GETNENV("SERVER_URL");
+
+export const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${redirect_uri}&prompt=consent&response_type=code&client_id=${GETNENV(
+    "google_client_id"
+)}&scope=openid%20email%20profile&access_type=offline`;
