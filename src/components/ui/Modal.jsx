d817fb4383475b188010b90586d7c432bcfeaf2c
emmanuel <badejoemmanuel805@gmail.com>
2025-02-24 11:21:54 -0800

    Refactor Profile Page and  uploading of Profile Pics

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const Modal = () => {
-  return (
-    <div className='backdrop-blur-3xl inset-0 z-[999999]'>Modal</div>
-  )
+import React, { Children } from 'react'
+import { twMerge } from 'tailwind-merge'
+const Modal = ({ children, className, onClick }) => {
+      return (
+            <div onClick={onClick} className={twMerge('backdrop-blur-md bg-white/20 inset-0 z-[999999] fixed', className)}>{children || ""}</div>
+      )
 }
 
 export default Modal
